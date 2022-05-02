dcddd8fbf058d3691654046edcf01fb1ab9ad8c0
aleksandrbarinov <barabarinov@gmail.com>
2022-05-03 01:20:45 +0300

    fixed handlers, functions. Corrected text and cleaned up the code

---
+++ app/handlers/donate.py
@@ -0,0 +1,33 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
+
+from app.information import text_bank_details, text_donate
+from telegram.ext import CallbackContext
+
+# Links
+LIQPAY_LINK = 'https://www.liqpay.ua/checkout/skeemanschurch'
+CRYPTO_LINK = 'https://skeemans.com/donate_crypto'
+
+reply_keyboard_submenu = [[InlineKeyboardButton('LiqPay 🔗', url=LIQPAY_LINK)],
+                          [InlineKeyboardButton('Crypto 🔗', url=CRYPTO_LINK)],
+                          [InlineKeyboardButton('SWIFT 🔗', callback_data='swift')]]
+reply_keyboard_donates = InlineKeyboardMarkup(reply_keyboard_submenu)
+
+
+def main_donate(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        text=text_donate,
+        reply_markup=reply_keyboard_donates,
+        parse_mode=ParseMode.MARKDOWN
+    )
+
+
+def swift(update: Update, context: CallbackContext):
+    query = update.callback_query
+    query.answer()
+
+    context.bot.edit_message_text(
+        chat_id=query.message.chat_id,
+        message_id=query.message.message_id,
+        text='\n'.join(text_bank_details),
+        parse_mode=ParseMode.MARKDOWN_V2
+    )
--- main.py
+++ main.py
@@ -1,54 +1,101 @@
-# import logging
-# import os
-# from dotenv import load_dotenv
-# from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
-# from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
-#
-# from app.information import INFORMATION
-#
-# logging.basicConfig(
-#     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-# )
-# logger = logging.getLogger(__name__)
-#
-#
-# def start(update, _):
-#     keyboard = [
-#         [
-#             InlineKeyboardButton('Дивитись ONLINE', callback_data='Online'),
-#         ],
-#         [
-#             InlineKeyboardButton('Церква у військовий час', callback_data='Церква у військовий час'),
-#         ],
-#         [
-#             InlineKeyboardButton('Залишити молитовну потребу', callback_data='Молитовні потреби'),
-#         ],
-#         [
-#             InlineKeyboardButton('Розклад служінь', callback_data='Розклад служінь'),
-#         ],
-#     ]
-#     reply_markup = InlineKeyboardMarkup(keyboard)
-#     update.message.reply_text('Зробіть вибір будь ласка:', reply_markup=reply_markup)
-#
-#
-# def button(update, _):
-#     # reply_keyboard = [['Go back']]
-#     query = update.callback_query
-#     variant = query.data
-#     query.answer()
-#     query.edit_message_text(text=f"{INFORMATION[variant]}", parse_mode=ParseMode.MARKDOWN)
-#     update.effective_message.reply_text('*Натисніть сюди --> `/menu` щоб перейти до головного меню*', parse_mode=ParseMode.MARKDOWN)
-#     update.effective_message.reply_text()
-#
-#
-# if __name__ == '__main__':
-#     load_dotenv()
-#     updater = Updater(token=os.getenv('TOKEN'), use_context=True)
-#     dispatcher = updater.dispatcher
-#
-#     updater.dispatcher.add_handler(CommandHandler('menu', start))
-#     updater.dispatcher.add_handler(CallbackQueryHandler(button))
-#     # updater.dispatcher.add_handler(CommandHandler('help', help_command))
-#
-#     updater.start_polling()
-#     updater.idle()
+import logging
+import os
+from dotenv import load_dotenv
+
+from app.information import text_wartime, text_bucha, text_live, text_hello
+from app.handlers.donate import main_donate, swift
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, ParseMode, Update
+from telegram.ext import (
+    Updater,
+    CommandHandler,
+    CallbackContext,
+    CallbackQueryHandler,
+    MessageHandler,
+    Filters,
+)
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+# Links
+PRAY_LINK = 'https://skeemans.com/pray'
+ONLINE_LINK = 'https://www.youtube.com/SKEEMANSCHURCH/live'
+
+# buttons
+reply_keyboard_menu = [['Служіння в Бучі ❤️', 'Церква під час війни 🇺🇦'],
+                       ['Підтримати (Donate) ✊🏼', 'Молитовна потреба 🙏🏻'],
+                       ['Недільне служіння 💒', 'Cлужіння LIVE 🔴']]
+reply_keyboard_main = ReplyKeyboardMarkup(reply_keyboard_menu, one_time_keyboard=False)
+
+reply_keboard_pray = [[InlineKeyboardButton('Написати 🔗', url=PRAY_LINK)]]
+reply_keboard_praylink = InlineKeyboardMarkup(reply_keboard_pray)
+
+reply_keboard_online = [[InlineKeyboardButton('Дивитись на YouTube 🔗', url=ONLINE_LINK)]]
+reply_keboard_onlinelink = InlineKeyboardMarkup(reply_keboard_online)
+
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        text_hello, reply_markup=reply_keyboard_main, parse_mode=ParseMode.MARKDOWN
+    )
+
+
+def get_help(update: Update, context: CallbackContext):
+    update.message.reply_text('\n\n'.join(text_bucha), parse_mode=ParseMode.MARKDOWN_V2)
+    update.message.reply_location(latitude=50.55355869171877, longitude=30.241997608604798)
+
+
+def online(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        text_live,
+        reply_markup=reply_keboard_onlinelink,
+        parse_mode=ParseMode.MARKDOWN_V2
+    )
+
+
+def church_in_wartime(update: Update, context: CallbackContext):
+    update.message.reply_text(f'*ЗА КОЖНОЮ ДОБРОЮ СПРАВОЮ СТОЯТЬ ЛЮДИ!*\n🙏🏼🇺🇦\n', parse_mode=ParseMode.MARKDOWN)
+    update.message.reply_photo(
+        caption=text_wartime,
+        photo=open('pics/web_war.png', 'rb'),
+        parse_mode=ParseMode.MARKDOWN
+    )
+
+
+def pray_request(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        text=f'{"[ТУТ](https://skeemans.com/pray)"} ви можете залишити молитовну потребу, і церква буде молитися за неї разом із вами 🙏🏼',
+        reply_markup=reply_keboard_praylink,
+        parse_mode=ParseMode.MARKDOWN
+    )
+
+
+def service_schedule(update: Update, context: CallbackContext):
+    update.message.reply_text('\n'.join(['_13:00 \- зібрання в церкві_',
+                                         '_16:00 \- онлайн служіння_',
+                                         ]), reply_markup=reply_keboard_onlinelink, parse_mode=ParseMode.MARKDOWN_V2)
+    update.message.reply_text('_Київ, вул\.Стеценка 30, лінія 4_', parse_mode=ParseMode.MARKDOWN_V2)
+    update.message.reply_location(latitude=50.496194103883155, longitude=30.372488286210967)
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    updater = Updater(token=os.getenv('TOKEN'), use_context=True)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(MessageHandler(Filters.regex('^Служіння в Бучі ❤️$') & ~Filters.command, get_help))
+    dispatcher.add_handler(MessageHandler(Filters.regex('^Cлужіння LIVE 🔴$') & ~Filters.command, online))
+    dispatcher.add_handler(
+        MessageHandler(Filters.regex('^Церква під час війни 🇺🇦$') & ~Filters.command, church_in_wartime))
+    dispatcher.add_handler(
+        MessageHandler(Filters.regex('^Молитовна потреба 🙏🏻$') & ~Filters.command, pray_request))
+    dispatcher.add_handler(MessageHandler(Filters.regex('^Недільне служіння 💒$') & ~Filters.command, service_schedule))
+    dispatcher.add_handler(MessageHandler(Filters.regex('^Підтримати \(Donate\) ✊🏼$') & ~Filters.command, main_donate))
+    dispatcher.add_handler(CallbackQueryHandler(swift, pattern='swift'))
+
+    updater.start_polling()
+    updater.idle()
